981b64f6f7f0ecf547b4c88565cafe01c437cae5
LevshinaA <113028419+LevshinaA@users.noreply.github.com>
2022-11-29 10:48:22 +0300

    Update floyd_Levshia.cpp

--- floyd_Levshia.cpp
+++ floyd_Levshia.cpp
@@ -71,7 +71,7 @@ vector <vector<bool>> algoritm_floyda(int n)
 	a = form_matrix(n);
 
 	for (int k = 0; k < n; k++)
-		for (int i = 0; i < n; k++)
+		for (int i = 0; i < n; i++)
 			for (int j = 0; j < n; j++)
 				a[i][j] = min (a[i][j], a[i][k] + a[k][j]);
 
