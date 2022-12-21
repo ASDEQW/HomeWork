87754df28c8b9265359ff3fc86d585df6f3bd36b
elyawg3 <112977658+elyawg3@users.noreply.github.com>
2022-12-21 14:36:40 +0300

    Add files via upload

---
+++ HomeWork/ford-bellman.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include "utility.h"
+
+using namespace std;
+
+
+
+vector <vector<int>> algoritm_forda_belmana(vector <vector<int>>& a)
+{
+	int n = a.size();
+	const int INF = 1e9;
+	vector <vector<int> > Mas_rast(n, vector<int>(10000, INF));
+	Mas_rast[0][0] = 0;
+	for (int k = 1; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			Mas_rast[k][i] = Mas_rast[k - 1][i];
+			for (int j = 0; j < n; j++) {	
+				if (Mas_rast[k - 1][j] + a[j][i] < Mas_rast[k][i]) {
+					Mas_rast[k][i] = Mas_rast[k - 1][j] + a[j][i];
+				}
+			}
+		}
+	}
+	return Mas_rast;
+}
